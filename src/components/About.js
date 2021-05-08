b81d7e582ee628a82bf5458d445d5ffc137a94e6
Brian Moranga <brian.moranga@infocap.co.ke>
2021-05-08 21:17:51 +0300

    Adds about me

--- src/components/About.js
+++ src/components/About.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { useStaticQuery, graphql } from "gatsby";
+import { useStaticQuery, graphql, Link } from "gatsby";
 import Img from "gatsby-image";
 import styles from "./About.module.scss";
 
@@ -18,33 +18,53 @@ const About = () => {
   `);
   return (
     <>
-      <h1>Hi, I'm Brian, a software developer from Nairobi, Kenya!</h1>
+      <h1>Hi, I'm Brian, a software engineer from Nairobi, Kenya!</h1>
       <div className={styles.aboutMeContainer}>
         <div className={styles.aboutImage}>
           <Img fluid={data.file.childImageSharp.fluid} alt="brian" />
           <div className={styles.sectionContainer}>
             <h2>About me</h2>
-            <p></p>
+            <p>
+              Here's a little about me. I love software engineering particularly
+              the backend. I got into this out of pure curiosity and ended up
+              being completely engrossed by it. I don't have much going on
+              outside of development. I love reading non-fiction books, just
+              recently started learning to draw. In my downtime, I play video
+              games or watch anime. My current favorite TV show is Attack On
+              Titan. I'm horrible at singing. I currently work as a contract
+              backend developer at{" "}
+              <a href="https://crowdbotics.com" target="_blank">
+                Crowdbotics
+              </a>{" "}
+              and finishing my degree in BSC. Mathematics with computing at
+              Kisii University, Kenya. You can follow me on Instagram. I'll be
+              posting weekly Gists on my{" "}
+              <a href="https://www.instagram.com/morangcode/" target="_blank">
+                Instagram
+              </a>{" "}
+              Story.
+            </p>
           </div>
         </div>
         <div className={styles.sectionContainer}>
           <h2>About this blog</h2>
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-            culpa qui officia deserunt mollit anim id est laborum.
+            I strive to be a better developer every day. Some of the recommended
+            ways to achieve this are by teaching and journaling progress. This
+            blog is meant to serve this purpose by acting as a platform for me
+            to track and measure my progress as I learn and for you to learn
+            various software engineering topics with real-world examples. We
+            will discuss algorithms & data structures, system design &
+            architecture, databases, DevOps, and tackle emerging technologies.
+            Join me in this journey, and let us learn together 🙂.
           </p>
         </div>
 
         <div className={styles.sectionContainer}>
-          <h2>Questions?</h2>
+          <h2>Get in touch</h2>
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            Have any questions or would like to say hi 👋,{" "}
+            <Link to="/contact">contact me!</Link>
           </p>
         </div>
       </div>
